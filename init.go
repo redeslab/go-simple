27b3f4e5ca800ae188c2d1ac969c670790473b26
hopwesley <ws@sinoc.io>
2022-05-10 18:44:45 +0800

    infura info

--- init.go
+++ init.go
@@ -60,14 +60,14 @@ func initMiner(_ *cobra.Command, _ []string) {
 
 	defaultSys.ECfg[com.RopstenNetworkId] = &com.EthereumConfig{
 		NetworkID:   com.RopstenNetworkId,
-		EthApiUrl:   "https://ropsten.infura.io/v3/d64d364124684359ace20feae1f9ac20",
+		EthApiUrl:   "https://kovan.infura.io/v3/56bf070cd1714103b3bd40e1da1edf86",
 		MicroPaySys: common.HexToAddress("0x72d5f9f633f537f87ef7415b8bdbfa438d0a1a6c"),
 		Token:       common.HexToAddress("0xad44c8493de3fe2b070f33927a315b50da9a0e25"),
 	}
 
 	defaultSys.ECfg[com.MainNetworkId] = &com.EthereumConfig{
 		NetworkID:   com.MainNetworkId,
-		EthApiUrl:   "https://mainnet.infura.io/v3/d64d364124684359ace20feae1f9ac20",
+		EthApiUrl:   "https://mainnet.infura.io/v3/56bf070cd1714103b3bd40e1da1edf86",
 		MicroPaySys: common.HexToAddress("0x5eC8589C8832AdE0B45C608681bbaCEF517E7caD"),
 		Token:       common.HexToAddress("0x1999ac2b141e6d5c4e27579b30f842078bc620b3"),
 	}
